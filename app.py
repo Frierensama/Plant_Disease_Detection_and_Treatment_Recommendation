4258359145dc0663bf63311737db42efa4f6a9d2
code:/02 <rudeusgreyrat.zeta002@gmail.com>
2026-07-17 21:23:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import torch
 
 st.set_page_config(page_title='Plant Disease Detection and Treatment Reccomendation ☘️', page_icon='☘️', layout='wide')
 
-
+# cnn model load
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 cnn = CustomNN(38)
 cnn.load_state_dict(torch.load('models/model_v05.pth',map_location=device))
@@ -20,8 +20,8 @@ cnn.eval()
 menu = st.sidebar.radio(
     'Navigation',
     [
-    #     'Home',
-    #  'Model Comparision',
+     'Home',
+     'Model Comparision',
      'Predict Label'
      ]
 )
@@ -42,6 +42,9 @@ with open('pickle_items/models_eval_df.pkl','rb') as f:
 with open('pickle_items/misclassified_df.pkl','rb') as f:
     misclassified_df = pickle.load(f)
 
+with open('pickle_items/df_per_class_accuracy.pkl','rb') as f:
+    per_class_accuracy_df = pickle.load(f)
+
 # cnn losses and accuracies
 with open('pickle_items/cnn/train_losses_cnn.pkl','rb') as f:
     train_losses_cnn = pickle.load(f)
@@ -61,7 +64,7 @@ if menu == 'Home':
     st.write(''' 
         *This application is built using PyTorch. 
         I have used few pre-trained models too to compare the performance.
-        For deployment purposes, i've used CustomCNN (which i have made) over pre-trained models.*
+        For deployment purposes, i've used CustomCNN (which i have created) over pre-trained models.*
         ''')
     
     col1, col2, col3, col4 = st.columns(4)
@@ -101,6 +104,11 @@ if menu == 'Home':
     f2.metric('Validation Size','17572')
     f3.metric('Accuracy on Test (33 labels)', '100%')
 
+    st.header('Per Class Accuracy')
+    st.dataframe(per_class_accuracy_df)
+
+    st.write('*I have trained pre-trained models with default classification layout (ie. one Linear Layer -- input_features to num_classes). Resnet50 out-performed all models after 10 epochs. for deployment purposes im using my model.*')
+
 if menu =='Predict Label':
     uploaded_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
     if uploaded_file is not None:
@@ -115,6 +123,7 @@ if menu =='Predict Label':
         _,pred_label = torch.max(output,axis=1)
         pred = class_names[pred_label]
         c0,c1 , c2 = st.columns(3)
-        c0.metric
+
+        c0.metric('**Predicted Class Accuracy**', f'{per_class_accuracy_df['Accuracy_CNN'][int(pred_label)]:.3f}')
         c1.image(uploaded_file)
         c2.metric('**Plant Class**',pred)
